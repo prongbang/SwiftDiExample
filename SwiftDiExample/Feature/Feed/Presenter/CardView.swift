34589975a5c8dfd1914ae0d598701636808e8b95
prongbang <prongbang@gmail.com>
2019-12-10 21:18:16 +0700

    add corner radius

--- SwiftDiExample/Feature/Feed/Presenter/CardView.swift
+++ SwiftDiExample/Feature/Feed/Presenter/CardView.swift
@@ -37,6 +37,12 @@ struct CardView: View {
             }
             .padding()
         }
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
     }
 }
 
