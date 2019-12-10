ad74d8b3f610f0be544110ce187aefc606735507
prongbang <prongbang@gmail.com>
2019-12-11 00:30:39 +0700

    initial

---
+++ SwiftDiExample/View/ImageView.swift
@@ -0,0 +1,35 @@
+//
+//  ImageView.swift
+//  SwiftDiExample
+//
+//  Created by Endtry on 10/12/2562 BE.
+//  Copyright © 2562 Endtry. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+struct ImageView: View {
+    @ObservedObject var imageLoader:ImageLoader
+    @State var image:UIImage = UIImage()
+    
+    init(withURL url:String) {
+        imageLoader = ImageLoader(urlString:url)
+    }
+    
+    var body: some View {
+        VStack {
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }.onReceive(imageLoader.didChange) { data in
+            self.image = UIImage(data: data) ?? UIImage()
+        }
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView(withURL: "")
+    }
+}
